4b456e0b2cd26d1993c07dc560e4026ed910a3f4
Zuhair Ali <zuhairali20003@gmail.com>
2021-03-08 10:22:12 -0600

    Initial Commit

---
+++ Real Map Hackwich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Real Map Hackwich
+//
+//  Created by Student on 3/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Real Map Hackwich/Real_Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Map_HackwichApp.swift
+//  Real Map Hackwich
+//
+//  Created by Student on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Real_Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
