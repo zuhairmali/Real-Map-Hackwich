e630caa31d885da656b23075a61cc9c868765011
Zuhair Ali <zuhairali20003@gmail.com>
2021-03-08 10:49:36 -0600

    Setup initial SwiftUI Map

--- Real Map Hackwich/ContentView.swift
+++ Real Map Hackwich/ContentView.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+@State private var region = MKCoordinateRegion(
+    center: CLLocationCoordinate2D(
+        latitude: 42.15704,
+        longitude: -88.14812),
+    span: MKCoordinateSpan(
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05)
+    )
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
